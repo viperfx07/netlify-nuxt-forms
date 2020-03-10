7147a61e00c9ced64049ec83eb5e46b48d90120b
Vic <viperfx07@gmail.com>
2020-03-10 18:42:38 +1100

    test form

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,10 @@
 <template>
   <div class="container">
-    <div>
-      <h1>{{ teaName }}</h1>
-    </div>
-    <div>
-      <form
-        name="vue-tea"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="vue-tea" />
-        <div class="tea">
-          <h2>Pick a Tea</h2>
-          <label
-            v-for="tea in teaNames"
-            :key="tea"
-            :class="{
-              'tea-label': true,
-              checked: tea === chosenTea
-            }"
-          >
-            <input
-              id="tea"
-              v-model="chosenTea"
-              name="tea"
-              type="radio"
-              :value="tea"
-            />
-            <span>{{ tea }}</span>
-          </label>
-        </div>
-        <button>Submit</button>
-      </form>
-    </div>
+    <nuxt-link to="/contact">
+      Contact
+    </nuxt-link>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      chosenTea: 'Earl Grey',
-      teaTypes: {
-        'Earl Grey': 'London',
-        'Irish Breakfast': 'Dublin',
-        Chai: 'Bombay',
-        Rose: 'Atlantic City',
-        Matcha: 'Tokyo',
-        Rooibos: 'Cape Town',
-        'Yerba Mate': 'Montreal',
-        Green: 'Oregon Mist'
-      }
-    }
-  },
-  computed: {
-    teaName() {
-      return `${this.teaTypes[this.chosenTea]} Fog`
-    },
-    teaNames() {
-      return Object.keys(this.teaTypes)
-    }
-  }
-}
-</script>
 
 <style>
 .container {
